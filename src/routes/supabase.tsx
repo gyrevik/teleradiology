567ca29dfabfc8a6df480baa1132b38a41e35377
Alex Smotritsky <alex.smotritsky@gmail.com>
2024-06-05 17:56:56 -0400

    getting data

--- src/routes/supabase.tsx
+++ src/routes/supabase.tsx
@@ -1,8 +1,37 @@
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Database } from '../types/supabase'
+
+const url = import.meta.env.VITE_SUPABASE_PROJECT_URL || ''
+const anonKey = import.meta.env.VITE_SUPABASE_ANON_PUBLIC_KEY || ''
+
+const supabase = createClient<Database>(
+    url,
+    anonKey
+)
+
 export default function SupabasePage() {
-  return (
-    <>
-      <h1>Supabase page</h1>
-      This is a public page.
-    </>
-  );
+    const [countries, setCountries] = useState<Database['public']['Tables']['countries']['Row'][]>([])
+
+    useEffect(() => {
+        const fetchCountries = async () => {
+            const { data } = await supabase.from('countries').select('*')
+            setCountries(data || []) // Provide a default value of an empty array if data is null
+        }
+
+        fetchCountries()
+    }, [])
+
+    return (
+        <>
+            <h1>Supabase page</h1>
+            This is a public page with country data from Supabase.
+
+            <div>
+                {countries.map(country => (
+                    <p key={country.id}>{country.name}</p>
+                ))}
+            </div>
+        </>
+    );
 }
